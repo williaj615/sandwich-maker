acb260aff9060a508646323b51c1bfd74f194255
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-28 12:45:40 -0500

    created condiments array

---
+++ javascripts/components/condiment.js
@@ -0,0 +1,10 @@
+import utilities from "../helpers/utilities.js"
+
+const condiments = [
+    {id: 'condiment1', name: 'Mustard', price: .90 },
+    {id: 'condiment2', name: 'Mayo', price: .70 },
+    {id: 'condiment3', name: 'Honey Mustard', price: .60},
+    {id: 'condiment4', name: 'Oil', price: .80},
+    {id: 'condiment5', name: 'Salt and Pepper', price: .70}
+];
+

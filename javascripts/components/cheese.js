e305c7d7d54b59d0d208d0e0dc19e783eefcd987
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-28 12:33:24 -0500

    added cheese array

---
+++ javascripts/components/cheese.js
@@ -0,0 +1,9 @@
+import utilities from "../helpers/utilities.js"
+
+const cheeses = [
+    {id: 'cheese1', name: 'Cheddar', price: .90 },
+    {id: 'cheese2', name: 'American', price: .70 },
+    {id: 'cheese3', name: 'Provolone', price: .60},
+    {id: 'cheese4', name: 'Pepper Jack', price: .80},
+    {id: 'cheese5', name: 'Swiss', price: .70}
+]

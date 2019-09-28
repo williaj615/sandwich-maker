5f48077111b7816fd47932268ddf4bbb178bafcc
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-28 13:03:11 -0500

    created function to print veggie options

--- javascripts/components/veggie.js
+++ javascripts/components/veggie.js
@@ -7,3 +7,16 @@ const veggies = [
     {id: 'veggie4', name: 'Onion', price: .80},
     {id: 'veggie5', name: 'Peppers', price: .70}
 ];
+
+const printVeggieOptions = () => {
+    let domString = '';
+    for (let i = 0; i < veggies.length; i++) {
+     domString += `<div class="form-group form-check">
+    <input type="checkbox" class="form-check-input cheese" id="${veggies[i].id}">
+    <label class="form-check-label" for="${veggies[i].id}">${veggies[i].name}</label>
+  </div>`
+    };
+    utilities.printToDom('veggie-counter', domString);
+};
+
+export default {printVeggieOptions}

4fb26b995d6525807ff6fd48c9320d5cd9b8cec7
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-28 12:57:40 -0500

    created array of veggies

---
+++ javascripts/components/veggie.js
@@ -0,0 +1,9 @@
+import utilities from "../helpers/utilities.js"
+
+const veggies = [
+    {id: 'veggie1', name: 'Lettuce', price: .90 },
+    {id: 'veggie2', name: 'Tomato', price: .70 },
+    {id: 'veggie3', name: 'Pickle', price: .60},
+    {id: 'veggie4', name: 'Onion', price: .80},
+    {id: 'veggie5', name: 'Peppers', price: .70}
+];

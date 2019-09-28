c1194b5c1222ce8654401b08ae6190bffddb8043
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-28 12:22:32 -0500

    added function to print available meats

--- javascripts/components/meat.js
+++ javascripts/components/meat.js
@@ -1,4 +1,4 @@
-import utilities from "../helpers/utilities"\
+import utilities from "../helpers/utilities.js"
 
 const meats = [
     {id: 'meat1', name: 'Turkey', price: .90 },
@@ -7,3 +7,16 @@ const meats = [
     {id: 'meat4', name: 'Salami', price: .80},
     {id: 'meat5', name: 'Roast Beef', price: .70}
 ];
+
+const printMeatOptions = () => {
+    let domString = '';
+    for (let i = 0; i < meats.length; i++) {
+     domString += `<div class="form-group form-check">
+    <input type="checkbox" class="form-check-input cheese" id="${meats[i].id}">
+    <label class="form-check-label" for="${meats[i].id}">${meats[i].name}</label>
+  </div>`
+    };
+    utilities.printToDom('meat-counter', domString);
+};
+
+export default {printMeatOptions};

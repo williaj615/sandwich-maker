f1effe176a9faffa3486af021d60e3ae5e2372cd
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-28 12:37:51 -0500

    added function to print cheese options

--- javascripts/components/cheese.js
+++ javascripts/components/cheese.js
@@ -6,4 +6,17 @@ const cheeses = [
     {id: 'cheese3', name: 'Provolone', price: .60},
     {id: 'cheese4', name: 'Pepper Jack', price: .80},
     {id: 'cheese5', name: 'Swiss', price: .70}
-]
+];
+
+const printCheeseOptions = () => {
+    let domString = '';
+    for (let i = 0; i < cheeses.length; i++) {
+     domString += `<div class="form-group form-check">
+    <input type="checkbox" class="form-check-input cheese" id="${cheeses[i].id}">
+    <label class="form-check-label" for="${cheeses[i].id}">${cheeses[i].name}</label>
+  </div>`
+    };
+    utilities.printToDom('cheese-counter', domString);
+};
+
+export default {printCheeseOptions};

eeba4f4d5572a5ef655144f130d914f00dace2c5
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-28 12:13:16 -0500

    created meats array

---
+++ javascripts/components/meat.js
@@ -0,0 +1,9 @@
+import utilities from "../helpers/utilities"\
+
+const meats = [
+    {id: 'meat1', name: 'Turkey', price: .90 },
+    {id: 'meat2', name: 'Ham', price: .70 },
+    {id: 'meat3', name: 'Chicken Salad', price: .60},
+    {id: 'meat4', name: 'Salami', price: .80},
+    {id: 'meat5', name: 'Roast Beef', price: .70}
+];
